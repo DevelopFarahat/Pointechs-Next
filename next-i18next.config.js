378106cf7ff4bbb179951cf81257bfe885f26875
divFarahat <mohamedfarahat366@gmail.com>
2023-04-25 00:21:41 +0200

    modifying next-i18nnext.config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+const path = require('path')
 module.exports = {
+  debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ar'],
     localeDetection: false,
   },
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+  localePath: path.resolve('./public/locales'),
 }
