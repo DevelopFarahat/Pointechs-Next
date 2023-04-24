a2191161159af89b883fa7fa00e8ad48f2a50568
divFarahat <mohamedfarahat366@gmail.com>
2023-04-24 23:17:14 +0200

    modyfing next-i18nnext

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'ar'],
-      domains: [
-        {
-          domain: 'pointechs-next.vercel.app',
-          defaultLocale: 'en',
-        },
-  
-      ],
-    },
-
-  }
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ar'],
+    localeDetection: false,
+    domains: [
+      {
+        domain: 'pointechs-next.vercel.app',
+        defaultLocale: 'en',
+        locales: ['en', 'ar'],
+      },
+    ],
+  },
+}
