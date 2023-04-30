9036970e0bbb47d5cdb19fd65c1e3bbbdcb5cdf0
divFarahat <mohamedfarahat366@gmail.com>
2023-04-30 10:28:24 +0300

    modyfing _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import JoinPointechs from "@/components/joinPointechs";
 import Contactus from "@/components/contactus";
 import 'bootstrap/scss/bootstrap.scss'
 import { appWithTranslation } from 'next-i18next'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const   App = ({ Component, pageProps })=> {
   return (
@@ -19,5 +20,5 @@ const   App = ({ Component, pageProps })=> {
     </>
   );
 }
-
+serviceWorkerRegistration.register();
 export default appWithTranslation(App)
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import PointechsApp from '@/components/pointechsApp'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 export default function Home() {
   const { t } = useTranslation('common');
   let {locale} = useRouter();
@@ -35,8 +35,9 @@ export default function Home() {
 
     </>
   )
+
 }
-serviceWorkerRegistration.register();
+
 export async function getServerSideProps({ locale }) {
   return {
     props: {
