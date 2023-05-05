801bb6c6f64e15fb537be0049373871db8922566
divFarahat <mohamedfarahat366@gmail.com>
2023-05-05 17:44:08 +0300

    next

--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@
 const { i18n } = require('./next-i18next.config')
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  skipWaiting: true,
-  runtimeCaching,
-  disable: process.env.NODE_ENV === 'development',
-   register: true,
+
 })
 
 const nextConfig = withPWA({
   reactStrictMode: false,
   i18n,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    disable: process.env.NODE_ENV === 'development',
+  },
 })
 
 module.exports = nextConfig
