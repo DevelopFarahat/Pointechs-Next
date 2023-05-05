707a1cf2b9d5ac45042a662bc51a77f425cd241a
divFarahat <mohamedfarahat366@gmail.com>
2023-05-05 11:39:39 +0300

    updating jsconfig

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,12 @@
-import Footer from "@/components/footer";
-import Header from "@/components/header";
-import "@/styles/globals.scss";
-import FooterTwo from "@/components/footerTwo";
-import JoinPointechs from "@/components/joinPointechs";
-import Contactus from "@/components/contactus";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import "../styles/globals.scss";
+import FooterTwo from "../components/footerTwo";
+import JoinPointechs from "../components/joinPointechs";
+import Contactus from "../components/contactus";
 import 'bootstrap/scss/bootstrap.scss'
 import { appWithTranslation } from 'next-i18next'
-
-import {ContextProvider} from "@/context/context";
+import {ContextProvider} from "../context/context";
 
 const   App = ({ Component, pageProps })=> {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 
-import PointchsHome from "@/components/pointchsHome";
-import Welcome from "@/components/welcom";
-import Features from "@/components/features";
-import WhyPointechs from "@/components/whyPointechs";
-import HowPointechsWorks from "@/components/howPointechsWorks";
-import PointechsApp from "@/components/pointechsApp";
+import PointchsHome from "../components/pointchsHome";
+import Welcome from "../components/welcom";
+import Features from "../components/features";
+import WhyPointechs from "../components/whyPointechs";
+import HowPointechsWorks from "../components/howPointechsWorks";
+import PointechsApp from "../components/pointechsApp";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
