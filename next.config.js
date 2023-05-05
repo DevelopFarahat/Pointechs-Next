707308c36c6ee23b6980e96079b1b5cca4c04011
divFarahat <mohamedfarahat366@gmail.com>
2023-05-05 09:22:29 +0300

    updating next.config

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const nextConfig = {
     dest:'public',
     register:true,
     skipWaiting:true,
-  //  disable: process.env.NODE_ENV === 'development',
+    scope:"/",
+    disable: process.env.NODE_ENV === 'development',
   }
 
 }
